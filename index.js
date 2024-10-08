0151706c6dde037fe6a256f628fbb5ccf86bfdd3
Lucas Alves Fonseca <lucasalvesfonseca147@gmail.com>
2024-10-08 14:39:37 -0300

    Feature: Insanidades Lista

--- index.js
+++ index.js
@@ -22,17 +22,18 @@ const commands = [
 // Função para registrar os comandos de guilda
 const rest = new REST({ version: "10" }).setToken(TOKEN);
 
+const GUILD_IDS = [process.env.GUILD_ID1, process.env.GUILD_ID2];
+
 (async () => {
   try {
     console.log("Start refreshing application (/) commands.");
-
-    // Para registrar comandos de guilda (instantâneo no servidor específico)
-    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
-      body: commands,
-    });
-
-    // Para registrar comandos globais (pode demorar até 1 hora para aparecer)
-    // await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+    
+    for (const guildId of GUILD_IDS) {
+      if (!guildId) throw new Error("GUILD_ID não está definido.");
+      await rest.put(Routes.applicationGuildCommands(CLIENT_ID, guildId), {
+        body: commands,
+      });
+    }
 
     console.log("Successfully reloaded application (/) commands.");
   } catch (error) {
@@ -40,12 +41,16 @@ const rest = new REST({ version: "10" }).setToken(TOKEN);
   }
 })();
 
+
+console.log("GUILD_IDS:", GUILD_IDS); // Para múltiplos GUILD_IDs
+
 // Criação do client
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
+
 
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
@@ -57,7 +62,7 @@ client.on("interactionCreate", async (interaction) => {
   if (interaction.commandName === "insanidade") {
     try {
       let ins = await insanidades();
-      await interaction.reply(`${ins}`);
+      await interaction.reply(`Insanidade: ${ins}`);
     } catch (error) {
       console.error(err)
       await interaction.reply("Ocorreu algum erro ao obter a Insanidade")
